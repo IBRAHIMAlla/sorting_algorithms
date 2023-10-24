4712cfbd14575f639326e775d9f3779f3be49e77
IBRAHIMAlla <brahimalla973@gmail.com>
2023-10-24 11:12:34 -0700

    sort.h

--- sort.h
+++ sort.h
@@ -30,5 +30,6 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void sw(listint_t **list, listint_t *node);
 
 #endif
